f4cb4a0573be78409a56de65aa504ada6f0b2542
BalaciSofia <klara.balaci@stud.ubbcluj.ro>
2026-02-27 01:31:23 +0200

    Remove ignored files from repository

--- semester 2/oop/practice_final3(star)/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_practice_final3_star__FILE C:/Users/balac/OneDrive/Desktop/oop/practice_final3(star)/cmake-build-debug/practice_final3_star_.exe)
-set(__QT_DEPLOY_TARGET_practice_final3_star__TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_practice_final3_star__RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
